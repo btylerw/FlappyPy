0e58032fb4cc5a05ca499e176b81f261cc85c336
Tyler Brown <brown.tyler.w@gmail.com>
2025-05-23 09:04:07 -0700

    Now renders two pipes, one flipped, with a gap between them for the player to navigate through

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from obstacles import Pipe
 
 # pygame setup
 pygame.init()
+pygame.display.set_caption("FlappyPy")
 SCREEN_HEIGHT = 1280
 SCREEN_WIDTH = 720
 
@@ -15,12 +16,19 @@ dt = 0
 
 # Creates game objects
 player = Character(100, 100, "img/pythonlogo.png")
-pipe = Pipe(500, 500, "img/pipe.png", SCREEN_HEIGHT, SCREEN_WIDTH)
+# Loading the pipe image to use in obstacles class
+pipe_image = pygame.image.load("img/pipe.png")
+# Creating a pipe image that is flipped upside down
+flipped_pipe_image = pygame.transform.flip(pipe_image, False, True)
+# Creating two pipes that will have a small space between them that the player must navigate through
+pipe = Pipe(1000, 600, pipe_image, SCREEN_HEIGHT, SCREEN_WIDTH)
+flipped_pipe = Pipe(1000, 0, flipped_pipe_image, SCREEN_HEIGHT, SCREEN_WIDTH)
 # Creating sprite groups for rendering/collision detection
 players = pygame.sprite.Group()
 pipes = pygame.sprite.Group()
 players.add(player)
 pipes.add(pipe)
+pipes.add(flipped_pipe)
 
 while running:
     # poll for events
--- obstacles.py
+++ obstacles.py
@@ -1,9 +1,11 @@
 import pygame
 
 class Pipe(pygame.sprite.Sprite):
-    def __init__(self, x, y, image_path, screen_height, screen_width):
+    def __init__(self, x, y, image, screen_height, screen_width):
         super().__init__()
-        self.image = pygame.image.load(image_path).convert_alpha()
+        # Set image to passed in image
+        self.image = image
+        # Saving initial x, y coordinates to reset later
         self.x = x
         self.y = y
         self.rect = self.image.get_rect()
@@ -13,9 +15,11 @@ class Pipe(pygame.sprite.Sprite):
         self.speed = 5
     
     def checkEdges(self):
+        # Resetting position when pipe goes off screen
         if self.rect.right < 0:
             self.rect.topleft = (self.x, self.y)
 
     def update(self):
+        # Move pipe to the left
         self.rect.x -= self.speed
         self.checkEdges()
